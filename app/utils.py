2586b1644dd77f41167a0a35386343066315d40c
Brad Aldous <72258175+Brudalaxe@users.noreply.github.com>
2024-12-10 21:35:56 +0000

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,7 @@ This file contains utility functions used across the app.
 Functions:
 1. validate_rating(): Validates that a given rating is a number between 1 and 5
 '''
+
 def validate_rating(rating):
     """
     Validate that the rating is between 1 and 5
